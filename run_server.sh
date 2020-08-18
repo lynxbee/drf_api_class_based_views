81e9b8c5f96268cb64380873cd20b6631460308f
Lynxbee Developer <social@lynxbee.com>
2020-08-18 22:16:32 +0530

    added sync db

--- run_server.sh
+++ run_server.sh
@@ -5,5 +5,6 @@ source env/bin/activate
 pip install django
 pip install djangorestframework
 python manage.py makemigrations
+python manage.py migrate --run-syncdb
 python manage.py migrate
 python manage.py runserver 192.168.0.106:8000
